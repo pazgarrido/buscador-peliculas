811a30e1cb626b8c3469fffb76b31eb6dc674d34
Paz <pazgarrido.es@gmail.com>
2022-04-02 13:27:51 +0200

    primer commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            title: "Movies",
+            movieData: {},
+            movieTitle: "Spider Man"
+        };
+    },
+    mounted (){
+        this.getMovie();
+    },
+
+    methods: {
+      async getMovie() {
+
+            const search = this.movieTitle.toLowerCase().replace(/ /g, "+");
+
+            console.log(search);
+
+            const data = await fetch (`http://www.omdbapi.com/?i=tt3896198&apikey=a117983e&t=${search}`);
+
+            const jsonData = await data.json();
+
+            this.movieData = jsonData;
+        },
+    },
+});
